98e0eaa8dc66585597d87d43242abc63be5a63d1
brandoll0530 <branollguzman@gmail.com>
2023-06-09 20:53:11 -0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // components
 import Banner from './components/Banner';
-import Header from './components/Header';
 import Nav from './components/Nav';
 import About from './components/About';
 import Services from './components/Services';
@@ -12,7 +11,6 @@ function App() {
 
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
-      <Header />
       <Banner />
       <Nav />
       <About />
